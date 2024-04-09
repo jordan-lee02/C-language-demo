8117bdc817528909b8b7d5675331360fd74cf953
jordanlee02 <jordan.lee02@outlook.com>
2024-04-10 00:04:59 +0100

    slightly fixed the loop to choose each part of the array and sperate them . Also added more debgguging outputs to follow the stored information

--- calculator.h
+++ calculator.h
@@ -26,32 +26,40 @@ static double runCalculation(double value1, double value2, char* operation) {
 }
 
 static double calculate(char *equation) {
-    char *value1;
-    char value2;
-    char *operation;
+    char value1[0];
+    char value2[0];
+    char operation[0];
+    double decVal1;
+    double decVal2;
 
     //This splits up the equation into individual elements
     int counter = 1;
     for (char *p = strtok(equation," "); p != NULL; p = strtok(NULL, " ")){
-        printf("loop: p: %s", p);
+        //printf("loop: p: %s\n", p);
         switch (counter) {
             case 1:
-                printf("here");
                 strcpy(value1, p);
-                printf("here23123 val1: %s", value1);
+                printf("First Value is: %s\n", value1);
+                break;
                 //define char array here and then move it out
             case 2:
                 strcpy(operation, p);
+                printf("Opertaion is: %s\n", operation);
+                break;
             case 3:
                 strcpy(value2, p);
+                printf("Second Value is: %s\n", value2);
+                break;
         }
         counter++;
     }
 
-    printf("here2");
+    printf("here2\n");
+    printf("Value 1 again: %s\n", value1);
 
-    double decVal1 = atof(value1);
-    double decVal2 = atof(value2);
+    decVal1 = atof(value1);
+    decVal2 = atof(value2);
+    printf("First value as a double: %lf\nSecond value as a double: %lf\n");
 
     double result = runCalculation(decVal1, decVal2, operation);
 
@@ -67,7 +75,7 @@ int initCalculator() {
     printf("In the format of number space operator space number, eg. 3 + 5\nPlease enter your calculation: ");
     scanf("%[^\n]", &equation);
 
-    printf("%s ", equation);
+    printf("%s\n", equation);
 
     double result = calculate(equation);
 
